ff68890a08e3064c7a9dccaa257948c50de41682
Raymond Gasper <raymondgasper@fastmail.com>
2025-10-22 21:39:22 -0400

    workin, ty bug recreated

--- webserver/mysite/app2/views.py
+++ webserver/mysite/app2/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def hello_app2():
-    print('hello from app2')
+def hello_app2(request):
+    html = """
+    <html>
+        <head>
+            <title>Hello</title>
+        </head>
+        <body>
+            <h1>Hello from app2</h1>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
--- webserver/mysite/mysite/urls.py
+++ webserver/mysite/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from app2.views import hello_app2
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("hello/", hello_app2),
 ]
