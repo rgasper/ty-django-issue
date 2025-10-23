19e1a0752a3da1abff6775d7a2b2d9673e9e0a80
Raymond Gasper <raymondgasper@fastmail.com>
2025-10-22 21:34:48 -0400

    setup

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ty-django-issue!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ webserver/mysite/app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hello_app2():
+    print('hello from app2')
